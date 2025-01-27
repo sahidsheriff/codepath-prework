a4602459cab4d5a18e3525471758a0073b406991
Sahid <109256658+sahidsheriff@users.noreply.github.com>
2025-01-27 14:05:15 -0800

    Added codepath prework file to repository

---
+++ KwikTests/KwikTests.swift
@@ -0,0 +1,17 @@
+//
+//  KwikTests.swift
+//  KwikTests
+//
+//  Created by SAHID AHMAD SHERIFF on 1/26/25.
+//
+
+import Testing
+@testable import Kwik
+
+struct KwikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
